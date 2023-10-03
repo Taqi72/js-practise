03ae42c52a56d9a5ff6e1ee64b6fe2ae5e689f64
Taqi72 <146829692+Taqi72@users.noreply.github.com>
2023-10-03 13:05:51 +0000

    variable set

---
+++ basic/01variable.js
@@ -0,0 +1,15 @@
+const accountid = 12345
+let   accountEmail = "dummyemail@gmail.com"
+var   accountPassword = "abc124567"
+accountstate = "multan"
+
+accountEmail = "email@gmail.com"
+accountPassword = "212212121"
+accountstate = "lahore"
+/*
+const is the keyword to use for you never want a variable to change, 
+Variables declared by let are only available inside the block where they're defined.
+Variables declared by var are available throughout the function in which they're declared.
+*/
+console.log(accountid);
+console.log([accountid,accountEmail,accountPassword,accountstate])
